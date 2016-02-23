42caaa36d814526b70e7c6d100df360147559757
Siyu Han <hansiyurei@gmail.com>
2016-02-22 22:56:09 -0500

    upload week 3 array assignment ver.2

--- README.md
+++
@@ -1,4 +0,0 @@
-# SPRING2016-.MMP310
-
-### This is all my MMP 210 Works
-<li><a href="index.html">homepage</a></li>
--- week3/main.js
+++ week3/main.js
@@ -1,15 +1,11 @@
 var upload = document.getElementById("upload");
 var massage = document.getElementById("message");
+var reaction = [];
+var reactionElements = document.getElementsByClassName("reaction");
+
 upload.onclick = function() {
-
-    var reaction = [];
-
-    var reactionElements = document.getElementsByClassName("reaction")
-
     for (var i = 0; i < reactionElements.length;i++)
         reaction.push(reactionElements[i].value);
 
-
-
-    message.innerHTML = "Hello " + reaction[1] + " " +reaction[0] + ", from " + reaction[2] + ", welcome to my website.";
+    message.innerHTML = "Hello " + reaction[1] + " " +reaction[0] + ", from " + reaction[2] + ", welcome to my website of MMP310.";
 };
