dd203bcb2db5da169a018cc43aa9217479d83247
Siyu Han <siyu.han@f1006-25-34156.bmcc.cuny.edu>
2016-02-29 19:21:47 -0500

    ver3 of m8 assignment without shaking

--- week4/m8.js
+++ week4/m8.js
@@ -5,7 +5,28 @@ var reactionElements = document.getElementsByClassName("reaction");
 
 upload.onclick = function() {
     for (var i = 0; i < reactionElements.length;i++)
-        reaction.push(reactionElements[i].value);
+    reaction.push(reactionElements[i].value);
 
-    answer.innerHTML = "Hello " + reaction[0] + " " +reaction[1] + ", if your question is: "  + reaction[2] + ". The Migic 8 answer will be";
+    answer.innerHTML = "Hello " + reaction[0] + " " +reaction[1] + ", if your question is: "  + reaction[2] + ", please click the Maggic ball, you will get your answer.";
 };
+
+var result = ["Yes","Without a doubt, YES!", "Just do it!", "No", "Not today","My reply is no","Hmm, this is a hard question, ask again after 5 seconds", "Try again",];
+
+var ball = document.getElementById("ball");
+
+var message = document.getElementById("message");
+
+ball.onclick = function () {
+    console.log(result [0]);
+    var randomanswer = result[Math.floor(Math.random() * result.length)];
+    console.log(randomanswer);
+    message.innerHTML = "The Magic 8 says: " + randomanswer;
+}
+
+
+
+
+
+
+
+
