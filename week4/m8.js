a72d1d9d0a4a729289f56009f20d21d6019dde80
Siyu Han <hansiyurei@gmail.com>
2016-02-29 20:11:32 -0500

    add shake effect

--- week4/m8.js
+++ week4/m8.js
@@ -7,7 +7,7 @@ upload.onclick = function() {
     for (var i = 0; i < reactionElements.length;i++)
     reaction.push(reactionElements[i].value);
 
-    answer.innerHTML = "Hello " + reaction[0] + " " +reaction[1] + ", if your question is: "  + reaction[2] + ", please click the Maggic ball, you will get your answer.";
+    answer.innerHTML = "Hello " + reaction[0] + " " +reaction[1] + ", if your question is: "  + reaction[2] + " Please click the Maggic ball, you will get your answer.";
 };
 
 var result = ["Yes","Without a doubt, YES!", "Just do it!", "No", "Not today","My reply is no","Hmm, this is a hard question, ask again after 5 seconds", "Try again",];
@@ -22,8 +22,6 @@ ball.onclick = function () {
     console.log(randomanswer);
     message.innerHTML = "The Magic 8 says: " + randomanswer;
 }
-
-
 
 
 
