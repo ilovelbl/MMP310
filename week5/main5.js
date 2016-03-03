ea5aee52cbec8e3775bc499a2fbf2e9dc4ce4ee9
Siyu Han <siyu.han@f1006-25-34156.bmcc.cuny.edu>
2016-03-03 12:56:46 -0500

    week5 assignment with bugs (js innerHtml doesn't work)

---
+++ week5/main5.js
@@ -0,0 +1,8 @@
+var yn = document.getElementById("yn");
+var answer = document.getElementById("answer");
+
+question.onclick = function() {
+
+    answer.innerHTML = "Hello " + reaction[0] + " " +reaction[1] + ", if your question is: "  + reaction[2] + ", please click the Maggic ball, you will get your answer.";
+};
+
