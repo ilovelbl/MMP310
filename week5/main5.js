123a224121f64af9c7f122b9317e5b21d30663bc
Siyu Han <siyu.han@f1006-25-34156.bmcc.cuny.edu>
2016-03-07 19:21:12 -0500

    should be limit the unlimited r++/w++

--- week5/main5.js
+++ week5/main5.js
@@ -1,33 +1,64 @@
 var yn = document.getElementById("yn");
 yn.onclick = function() {
-    //console.log(this);
+    //console.log();
     var name = document.getElementById("name").value;
-    //console.log(name);
+    console.log("worl");
     var message = document.getElementById("message");
     message.innerHTML = "OK " + name + ", let's begain";
 };
 
 ///////if else statement////////
 
-var x = 0;
+var r = 0;
 var y = 5;
-var c;
-var right;
-var addscore = function () {
-    if (right.onclick) { x = ++;}
-};
+var w = 0;
+
+var right = document.getElementsByClassName("right");
+for (var i = 0; i < right.length; i++) {
+    right[i].onclick = function(){
+        r++;
+        if (r >= 5) {
+            r == 5;
+        }
+        console.log(r + " right score");
+    }
+}
+
+var wrong = document.getElementsByClassName("wrong");
+for (var j = 0; j < wrong.length; j++){
+    wrong[j].onclick = function(){
+        w++;
+        console.log(w+" wrong score")
+    }
+}
+
+
+//var e = 0;
+//var ex = document.getElementById("ex");
+//ex.onclick = function() {
+//    e++;
+//    console.log(e);
+//}
+//
+
+
 
 var submit = document.getElementById("submit");
+
 submit.onclick = function() {
-    var commen = function (x,y) {
-        if (x = y){
-            c = "good";
-        }else if (x < y){
-            c = "not bad";
+    var commen = document.getElementById("commen");
+    if (r >= y){
+            commen = "good";
+        console.log(r);
+
+        }else if (r < y && r >= 3){
+            commen = "not bad";
+        }else if (r < 3){
+            commen = "try hard";
         }
-    };
+
     var result = document.getElementById("result");
-    result,innerHeight = c + "thank you";
+    result.innerHTML = commen + " you have " + r + "/5 right, " + w  +" /5 wrong, " + " thank you";
 };
 
 
