aea94b25265e85dcfc9b9f295cba09ae15d84b87
Siyu Han <siyu.han@f1006-25-34156.bmcc.cuny.edu>
2016-03-09 19:03:11 -0500

    class excervise of comprehention

---
+++ week6/main6.js
@@ -0,0 +1,19 @@
+var c = document.getElementById("container");
+c.style.color = "green";
+var img = new Image();
+img.src = "https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg";
+img.style.width = "200px";
+var elem = document.createElement("div");
+elem.appendChild(img);
+c.appendChild(elem);
+
+
+
+
+var img2 = new Image();
+img2.src = "http://purrfectcatnames.com/wp-content/uploads/2013/11/Gray-Cat-MorgueFile-Nov16th-2013.jpg";
+img2.style.width = "200px";
+var newdiv = document.createElement("div");
+newdiv.className = "box";
+newdiv.appendChild(img2);
+c.appendChild(newdiv);
