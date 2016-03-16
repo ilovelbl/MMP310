86fb6499c502b5da146eae8eda4d87a0b3cf9b89
Siyu Han <siyu.han@f1006-25-34156.bmcc.cuny.edu>
2016-03-16 19:19:36 -0400

    sss

--- week7/main7.js
+++ week7/main7.js
@@ -2,14 +2,16 @@ var pletters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@
 var password = "";
 
 
-var massage = document.getElementById("message");
+var message = document.getElementById("message");
+console.log(message);
 var submit = document.getElementById("submit");
 console.log(submit);
+
 submit.onclick = function() {
     console.log("onclick");
 
-    for(var i = 0; i < pletters.length; i++) {
-        password = pletters[Math.floor(Math.random() * pletters.length)];
-    }
-    message.innerHTML = password;
+//    for(var i = 0; i < pletters.length; i++) {
+//        password = pletters[Math.floor(Math.random() * pletters.length)];
+//    }
+    message.innerHTML = password+ "sdsd";
 }
