a8c722b5bc675983c8ce17d76944ac4743025a89
Siyu Han <siyu.han@f1006-25-34156.bmcc.cuny.edu>
2016-03-21 19:09:07 -0400

    messi

--- week7/main7.js
+++ week7/main7.js
@@ -1,17 +1,29 @@
-var pletters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+var pletters =  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
 var password = "";
 
+var ur = document.getElementById("ur").value;
 
 var message = document.getElementById("message");
 console.log(message);
+
 var submit = document.getElementById("submit");
 console.log(submit);
 
 submit.onclick = function() {
-    console.log("onclick");
+    console.log("submit");
 
-//    for(var i = 0; i < pletters.length; i++) {
-//        password = pletters[Math.floor(Math.random() * pletters.length)];
-//    }
-    message.innerHTML = password+ "sdsd";
+    message.innerHTML =  ur + "you already signed up";
+}
+
+var ps = document.getElementById("ps");
+
+ps.onclick = function() {
+    console.log(ps);
+
+    for(var i = 0; i < pletters.length; i++) {
+        password += pletters[Math.floor(Math.random() * pletters.length)];
+        password = document.getElementById("password").value;
+    }
+
+
 }
