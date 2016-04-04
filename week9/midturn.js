123c3aeac5a03f50d195240f1f461d52c53e63c1
Siyu Han <siyu.han@f1006-25-34156.bmcc.cuny.edu>
2016-04-04 19:17:15 -0400

    MIDTURN VER2

--- week9/midturn.js
+++ week9/midturn.js
@@ -1,18 +1,43 @@
 var startButton = document.getElementById("start");
-var quizdiv = document.createElement("div");
-var question = document.getElementById("input");
-var counntDisplay= document.getElementById("countdowm")
+var quiz = document.getElementById("quiz");
 
-//console.log(startButton,quiz,input);
+var rightAnswer = document.getElementsByClassName("right");
+var wrongAnswer = document.getElementsByClassName("wrong");
+//var rightAnswer = document.getElementsByTagName("input");
 
-quizdiv.style.display = "none";
+var countDisplay = document.getElementById("countdown");
 
-startButton.addEventListener("click",function(){
-    quiz.style.display = "black";
+quiz.style.display = "none";
+
+startButton.addEventListener("click", function() {
+
+    var count = 5;
+    var countdown = function() {
+        if (count == 0) {
+            clearInterval(countdownInterval);
+            var checked = 0;
+            for (var i = 0; i < rightAnswer.length; i++) {
+                if (rightAnswer[i].checked) {
+                    checked++;
+                }
+                startButton.style.display = "block";
+                startButton.innerHTML = "Try again.";
+            }
+
+//            for (var j = 0; j < wrongAnswer.length; j++) {
+//                if (wrongAnswer[i].checked) {
+//                    checked--;
+//                }
+//
+            countDisplay.innerHTML = "Out of time. Your score is " + checked + " / " + rightAnswer.length;
+        } else {
+            countDisplay.innerHTML = "Time to complete quiz: " + count;
+            count--;
+        }
+    };
+    var countdownInterval = setInterval(countdown, 1000);
+
+
+    quiz.style.display = "block";
     this.style.display = "none";
-
-})
-
-var count = 60;
-
-var countdown =
+});
