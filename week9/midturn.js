01d4b9c360c6e456061d28029d39f2d2279a9281
Siyu Han <hansiyurei@gmail.com>
2016-04-06 16:46:45 -0400

    midturm ver.submit

--- week9/midturn.js
+++ week9/midturn.js
@@ -1,17 +1,21 @@
 var startButton = document.getElementById("start");
 var quiz = document.getElementById("quiz");
-
 var rightAnswer = document.getElementsByClassName("right");
 var wrongAnswer = document.getElementsByClassName("wrong");
-//var rightAnswer = document.getElementsByTagName("input");
 
+var count = 50;//out of the click fuction means this var can be controled by submitButton
 var countDisplay = document.getElementById("countdown");
 
 quiz.style.display = "none";
 
 startButton.addEventListener("click", function() {
 
-    var count = 5;
+    var submitButton = document.getElementById("submit");
+    submitButton.onclick = function() {
+        count = 1;
+    }
+//    console.log(document.getElementById("submit"));
+
     var countdown = function() {
         if (count == 0) {
             clearInterval(countdownInterval);
@@ -24,20 +28,14 @@ startButton.addEventListener("click", function() {
                 startButton.innerHTML = "Try again.";
             }
 
-//            for (var j = 0; j < wrongAnswer.length; j++) {
-//                if (wrongAnswer[i].checked) {
-//                    checked--;
-//                }
-//
-            countDisplay.innerHTML = "Out of time. Your score is " + checked + " / " + rightAnswer.length;
+            countDisplay.innerHTML = "Time over. Your score is " + checked * 10 + " / " + rightAnswer.length + "0";
         } else {
             countDisplay.innerHTML = "Time to complete quiz: " + count;
             count--;
         }
     };
     var countdownInterval = setInterval(countdown, 1000);
-
-
     quiz.style.display = "block";
     this.style.display = "none";
 });
+
